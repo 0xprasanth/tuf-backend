b55721976d1a0af2f7b78e6bc473d4ed43cca4fb
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-03-20 22:20:56 +0530

    add "/"

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.json(['Hello from /'])
+})
+
+
 app.get("/submission", async (req, res) => {
   // const users = await getUsers()
   /* supabase */
